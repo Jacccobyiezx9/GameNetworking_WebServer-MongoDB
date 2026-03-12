07c16113b06c69c9eaf8fa2b4b03a684ba90fc2f
Jacccobyiezx9 <167274314+Jacccobyiezx9@users.noreply.github.com>
2026-03-12 20:44:17 +0800

    Added login and player stats

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const dotenv = require ('dotenv');
 
 const{
-    createPlayer
+    createPlayer,
+    login,
+    updateScore
 }   = require('./controllers/playerController');
 
 
@@ -27,7 +29,9 @@ app.put('/', (req, res)=>{
     res.json({message: 'Welcome to Game Networking'});
 });
 
-app.post('/players', createPlayer);
+app.put('/players/:id', updateScore);
+app.post('/players/register', createPlayer);
+app.post('/players/login', login);
 
 app.delete('/', (req, res)=>{
     res.json({message: 'Welcome to Game Networking'});
